0b0666ac9b1b34ee1b4df3fafead0a852defa5ee
feihuan1 <ericrealtor1997@gmail.com>
2024-02-29 16:41:15 -0800

    Initial Commit

---
+++ apartment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  apartment
+//
+//  Created by Feihuan Peng on 2/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
